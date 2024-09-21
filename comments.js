d0c42cd00a188c67bcc6515c4b58f2e6f8962e2a
koja-amir <164602598+koja-amir@users.noreply.github.com>
2024-09-21 20:19:06 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,13 @@
+// Create web server
+// Create a web server that listens on port 3000. 
+// When you visit http://localhost:3000/ in your browser, the server should respond with a web page that displays the following message:
+// "Welcome to the comments page!"
+// The server should listen on port 3000. Note: you should use the http module to create the server.
+// var http = require('http');
+// var server = http.createServer(function(request, response){
+//     response.writeHead(200, {'Content-Type': 'text/plain'});
+//     response.write('Welcome to the comments page!');
+//     response.end();
+// });
+// server.listen(3000);
+// console.log('Server is listening on port 3000');
